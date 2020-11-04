1a80dee4ca39c9a65c63b0f7b4f007b84ae43531
bigsky-park <ptm302@gmail.com>
2020-11-04 21:23:14 +0900

    Add logs to each method

--- internal/blog/blog.go
+++ internal/blog/blog.go
@@ -3,6 +3,9 @@ package blog
 import (
 	"context"
 
+	"google.golang.org/grpc/peer"
+	"k8s.io/klog/v2"
+
 	pb "github.com/bigsky-park/go-grpc-example/api/blog"
 )
 
@@ -24,6 +27,9 @@ func NewBlogService(storage Storage) *BlogService {
 }
 
 func (b *BlogService) CreatePost(ctx context.Context, in *pb.CreatePostRequest) (resp *pb.CreatePostResponse, err error) {
+	p, _ := peer.FromContext(ctx)
+	klog.Infof("CreatePostRequest from %s, payload: (%#v)", p.Addr.String(), in)
+
 	post, err := b.storage.CreateBlog(in)
 	if err != nil {
 		return nil, err
@@ -34,6 +40,9 @@ func (b *BlogService) CreatePost(ctx context.Context, in *pb.CreatePostRequest) 
 }
 
 func (b *BlogService) GetPost(ctx context.Context, in *pb.GetPostRequest) (*pb.GetPostResponse, error) {
+	p, _ := peer.FromContext(ctx)
+	klog.Infof("GetPost from %s, payload: (%#v)", p.Addr.String(), in)
+
 	post, err := b.storage.GetBlog(in)
 	if err != nil {
 		return nil, err
@@ -44,6 +53,9 @@ func (b *BlogService) GetPost(ctx context.Context, in *pb.GetPostRequest) (*pb.G
 }
 
 func (b *BlogService) ListPosts(ctx context.Context, in *pb.ListPostsRequest) (*pb.ListPostsResponse, error) {
+	p, _ := peer.FromContext(ctx)
+	klog.Infof("ListPosts from %s, payload: (%#v)", p.Addr.String(), in)
+
 	posts, err := b.storage.ListPosts(in)
 	if err != nil {
 		return nil, err
